8c574d4702747e09fdb1ac61f901f74d0618851a
Cesar Vargas <cesar.vargas.aliaga@gmail.com>
2017-07-12 22:31:23 -0500

    adding two google api keys for server and browser

--- start.py
+++ start.py
@@ -10,67 +10,67 @@ app = Flask(__name__)
 
 
 def get_nearest_locations(base_location, black_list):
-	radius = 0.003
-	xl = math.sqrt(3) * radius / 2
+    radius = 0.003
+    xl = math.sqrt(3) * radius / 2
 
-	locations = [
-		(base_location[0] - radius, base_location[1]),
-		(base_location[0] - radius / 2, base_location[1] - xl),
-		(base_location[0], base_location[1] - radius),
-		(base_location[0] + radius / 2, base_location[1] - xl),
-		(base_location[0] + radius, base_location[1]),
-		(base_location[0] + radius / 2, base_location[1] + xl),
-		(base_location[0], base_location[1] + radius),
-		(base_location[0] - radius / 2, base_location[1] + xl),
-	]
+    locations = [
+        (base_location[0] - radius, base_location[1]),
+        (base_location[0] - radius / 2, base_location[1] - xl),
+        (base_location[0], base_location[1] - radius),
+        (base_location[0] + radius / 2, base_location[1] - xl),
+        (base_location[0] + radius, base_location[1]),
+        (base_location[0] + radius / 2, base_location[1] + xl),
+        (base_location[0], base_location[1] + radius),
+        (base_location[0] - radius / 2, base_location[1] + xl),
+    ]
 
-	values_found = []
-	for l in locations:
-		if l in black_list:
-			values_found.append(l)
+    values_found = []
+    for l in locations:
+        if l in black_list:
+            values_found.append(l)
 
-	if len(values_found):
-		for v in values_found:
-			locations.remove(v)
-	
-	return locations
+    if len(values_found):
+        for v in values_found:
+            locations.remove(v)
+
+    return locations
 
 
 def get_lowest_elevation(locations):
-	gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
-	result = gmaps.elevation(locations)
+    gmaps = googlemaps.Client(key=GOOGLE_API_KEY_SERVER)
+    result = gmaps.elevation(locations)
 
-	elevation = 10000
-	result_item = None
+    elevation = 10000
+    result_item = None
 
-	for r in result:
-		if r['elevation'] < elevation:
-			result_item = r
-			elevation = r['elevation']
+    for r in result:
+        if r['elevation'] < elevation:
+            result_item = r
+            elevation = r['elevation']
 
-	return (result_item['location']['lat'], result_item['location']['lng'])
+    return (result_item['location']['lat'], result_item['location']['lng'])
 
 
 def get_simulated_route(base_location, size):
-	location = base_location
-	route = [base_location]
-	black_list = [base_location]
+    location = base_location
+    route = [base_location]
+    black_list = [base_location]
 
-	for i in range(size):
-		locations = get_nearest_locations(location, black_list)
-		if len(locations) == 0:
-			break
+    for i in range(size):
+        locations = get_nearest_locations(location, black_list)
+        if len(locations) == 0:
+            break
 
-		black_list += locations
-		location = get_lowest_elevation(locations)
-		route.append(location)
+        black_list += locations
+        location = get_lowest_elevation(locations)
+        route.append(location)
 
-	return route
+    return route
 
 
 @app.route("/")
 def home():
-    return render_template('layout.html', key=GOOGLE_API_KEY)
+    return render_template('layout.html', key=GOOGLE_API_KEY_BROWSER)
 
 
 @app.route('/simulate', methods=['POST'])
@@ -82,6 +82,7 @@ def simulate():
     route = get_simulated_route(base_location, 20)
 
     return json.dumps(route)
+
 
 if __name__ == "__main__":
     app.run()
